a3a2fd3d1c6d2423ab99815a0cd8a71e07e2b0db
Jef-Dev <133253085+jefersonrodolpho@users.noreply.github.com>
2023-08-19 12:11:49 -0300

    Add files via upload

--- Exercicios.py
+++ Exercicios.py
@@ -11,11 +11,11 @@ profissao = input(f"\nSabe me dizer {nome} qual a sua Profissão?")
 print(profissao)
 print(f"\nQue legal, sabia que um {profissao} ganha bem?")
 print(f"\nVamos ver quanto você ganha hoje e o imposto que paga sobre seu salário")
-salario = int(input("Valor Salário:"))
-imposto = input("Imposto:")
+salario = float(input("Valor Salário:"))
+imposto = float (input("Imposto:"))
 
 if not imposto:
-    imposto = 27.5
+    imposto = float (27.5)
 else: 
     imposto = float(imposto)
 sal = salario - (salario*(imposto*0.01)) #observe que foi criada uma nova variavel para poder efetuar o calculo, chamada "sal"
@@ -24,16 +24,16 @@ print(sal)
 #######
 print("Vamos calcular novamente!")
 def CalculoSalario():
-    imposto = 27.
+    imposto = float (27.5)
     while imposto > 0.:
         imposto = input("Imposto ou (s) sair:")
         if not imposto:
-            imposto = 27.
+            imposto = float (27.5)
         elif imposto == 's':
             break
         else:
             imposto = float(imposto)
         print("Valor Final: {0}".format(salario - (salario*imposto*0.01)))
-salario = int(input("Valor Salario: "))
+salario = float(input("Valor Salario: "))
 CalculoSalario()
 print(f"\n Obrigado por utilizar o cálculo de imposto por este exercício")
